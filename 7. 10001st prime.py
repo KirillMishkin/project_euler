7ca8510f03d5263dba64d13be3c4449faadeafb5
Kirill Mishkin <mkv1005@gmail.com>
2020-04-04 21:25:04 +0300

    New04042020

--- 7. 10001st prime.py
+++ 7. 10001st prime.py
@@ -1,18 +1,14 @@
 import time
-
-
-def IsPrime(n):
-    div = 2
-    while n % div != 0:
-        div += 1
-    return div == n
-
+from math import sqrt
 
 def number_prime(i):
     num_prime = 0
     while True:
         i += 1
-        if IsPrime(i):
+        for j in range(2, int(sqrt(i)) + 1):
+            if i % j == 0:
+                break
+        else:
             num_prime += 1
             if num_prime >= 10001:
                 return print(i)
